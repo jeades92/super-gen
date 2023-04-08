0a49f503ce3f403a46fe751b6fd1b67971ed90b1
Jimmys Mac <eadesj25@gmail.com>
2023-04-08 17:05:40 -0400

    finished logic

--- index.js
+++ index.js
@@ -2,13 +2,33 @@ const inquirer = require("inquirer")
 const fs = require("fs")
 
 function showReadMe(data) {
+    function renderLicenseBadge(data) {
+        if(data === "MIT") {
+            return '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)'
+        }
+        if(data === "APACHE 2.0") {
+            return '[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)'
+        }
+        if(data === "GPL 3.0") {
+            return '[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)'
+        }
+        if(data === "BSD 3") {
+            return '[![License](https://img.shields.io/badge/License-BSD_3--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)'
+        }
+        if(data === "NONE") {
+            return ''
+        }
+    }    
+
     console.log("show read me function", data)
     return `# ${data.title}
-    
-    ## description
-    ${data.description}
-    
-    ## Table of Contents 
+
+${renderLicenseBadge(data.license)}
+
+## description
+${data.description}
+
+## Table of Contents 
 
 * [Installation](#installation)
 
@@ -20,9 +40,40 @@ function showReadMe(data) {
 
 * [Tests](#tests)
 
-* [Questions](#questions)`
+* [Questions](#questions)
+
+## Installation
+
+To install necssary dependencies run the following command: ${data.installation}
+
+## Usage
+
+${data.usage}
+
+## License
+
+This project is licensed under the ${data.license} License
+
+## Contributing
+
+${data.contributing}
+
+## Tests
+
+To run tests run the following command:
+
+${data.tests}
+
+## Questions
+
+If you have any questions please contact me at ${data.email}
+
+My github is located at [${data.username}](https://github.com/${data.username})
+
+`
 
 }
+
 
 function showQuestions() {
     inquirer.prompt([
@@ -63,6 +114,16 @@ function showQuestions() {
             name:"tests",
             message:"What's the test command?",
             default:"npm run test"
+        },
+        {
+            type:"input",
+            name:"username",
+            message:"What is your Github username?",
+        },
+        {
+            type:"input",
+            name:"email",
+            message:"What is your email address?",
         },
     ])
         .then((answers) => {
--- readme.md
+++ readme.md
@@ -1,9 +1,11 @@
-# 1
-    
-    ## description
-    2
-    
-    ## Table of Contents 
+# title 2
+
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+
+## description
+describe 2
+
+## Table of Contents 
 
 * [Installation](#installation)
 
@@ -16,3 +18,32 @@
 * [Tests](#tests)
 
 * [Questions](#questions)
+
+## Installation
+
+To install necssary dependencies run the following command: npm i
+
+## Usage
+
+one
+
+## License
+
+This project is licensed under the MIT License
+
+## Contributing
+
+two
+
+## Tests
+
+To run tests run the following command:
+
+npm run test
+
+## Questions
+
+If you have any questions please contact me at mail@mail.com
+
+My github is located at [jeades92](https://github.com/jeades92)
+
